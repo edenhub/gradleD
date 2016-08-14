dc9a2d12a396213e5c342d735f3ca667c7c5be7f
adam <adamchen.pub@gmail.com>
2016-08-14 18:07:39 +0800

    init

---
+++ gradle-first/src/main/java/com/adam/gradleD/p1/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.adam.gradleD.p1;
+
+/**
+ * Created by adam on 16-8-14.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Gradle");
+    }
+}
